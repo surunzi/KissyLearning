3ba874467b84fd2dbaa7921062a7cc6a89b8f843
surunzi <670785827@qq.com>
2014-06-05 13:02:08 +0800

    增加代码高亮

--- js/KissyLearning.js
+++ js/KissyLearning.js
@@ -1,3 +1,4 @@
+KISSY.ready(function (S) {
 KISSY.use(['node'], function (S, Node) {
 
 var $page, 
@@ -20,25 +21,40 @@ if (features) {
     features = {};
 }
 
-// 主页图标说明弹出
-if (features['homeIconInfo']) {
-    (function() {
-        var $intro = Node.one('#features .intro .inner'),
-            introShowed = false, introTimer = null;
-        Node.all('#features .feature').on('click', function () {
-            var text = Node.one(this).one('p').text();
-            $intro.text(text);
-            clearTimeout(introTimer);
-            introTimer = setTimeout(function () {
-                $intro.slideUp(0.2);
-                introShowed = false;
-            }, 10000);
-            if (!introShowed) {
-                $intro.slideDown(0.2);
-            }
-            introShowed = true;
-        });
-    })();
+/**
+ * 特性选择函数
+ * 如果name被该页面指定了，才执行相应的func
+ */
+function initFeature(name, func) {
+    if (features[name]) {
+        func();
+    }
 }
 
+// 主页图标说明弹出
+initFeature('homeIconInfo', function () {
+    var $intro = Node.one('#features .intro .inner'),
+        introShowed = false, introTimer = null;
+    Node.all('#features .feature').on('click', function () {
+        var text = Node.one(this).one('p').text();
+        $intro.text(text);
+        clearTimeout(introTimer);
+        introTimer = setTimeout(function () {
+            $intro.slideUp(0.2);
+            introShowed = false;
+        }, 10000);
+        if (!introShowed) {
+            $intro.slideDown(0.2);
+        }
+        introShowed = true;
+    });
+});
+
+// 代码高亮
+initFeature('prettify', function () {
+    Node.all('code').addClass('prettyprint');
+    prettyPrint();
+});
+
+});
 });
